41e092bdf16279102957b8325f80a1d35f6be9cd
Apiwat Lee <apiwatz.lee@gmail.com>
2025-04-03 23:55:37 +0700

    feat:about me

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -13,12 +14,26 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)';
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='' />
       </div>
-      <nav className=' w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll && 'bg-white/50 backdrop-blur-lg'
+        }`}
+      >
         <a href='#top'>
           <Image
             src={assets.logo}
@@ -27,7 +42,11 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            !isScroll && 'bg-white/50 shadow-sm backdrop-blur-sm'
+          } `}
+        >
           <li>
             <a className='font-ovo' href='#top'>
               Home
