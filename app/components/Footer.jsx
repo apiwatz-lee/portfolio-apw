50d60a0b1469dcbf0ef3d3a208f57b651fa0f600
apw <apiwatz.lee@gmail.com>
2026-01-16 11:34:46 +0700

    feat: update Footer component to include FavoriteIcon and modify connection message

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import EmailIcon from '@mui/icons-material/Email';
 import Divider from '@mui/material/Divider';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Footer = () => {
   return (
@@ -13,7 +14,9 @@ const Footer = () => {
       <Divider className='pt-4' />
       <div className='flex justify-between w-full text-sm text-gray-500 py-4'>
         <div>© {new Date().getFullYear()} APW. All rights reserved.</div>
-        <div>Connect with me</div>
+        <div className='items-center'>
+          Made with <FavoriteIcon fontSize='small' />
+        </div>
       </div>
     </div>
   );
